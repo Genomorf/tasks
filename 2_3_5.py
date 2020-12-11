84ae80d840b47c232b7e6b330747a1cc1aaeeca9
Sazhin Nikolay <55433916+Genomorf@users.noreply.github.com>
2020-12-11 13:28:59 +0300

    Create 2_3_5.py

---
+++ 2_3_5.py
@@ -0,0 +1,11 @@
+def find_2_3_5_number(n: int) -> int:
+    if n == 0:
+        return 0
+    res: list = [1]
+    num: int = 1
+    while len(res) != n:
+        num += 1
+        if num % 2 == 0 or num % 3 == 0 or num % 5 == 0:
+            res.append(num)
+    print(res)
+    return res[n-1]
